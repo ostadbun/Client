9e18394b5e27e0c609e1bd4af34bdbe785883c26
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-08-31 20:34:41 +0330

    init

---
+++ src/app/k/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./components/button";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Kazem is here </h1>
+      <ModeToggle />
+      <Button>hey kazem</Button>
+    </div>
+  );
+};
+
+export default Page;
