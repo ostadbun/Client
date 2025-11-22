e91f5513c4c3762c1dfae7357cd9011e5368c0e6
kazem <mohammadkazemfarahbakhsh5@gmail.com>
2025-11-22 21:36:49 +0330

    fixing theme

--- src/components/magicui/animated-theme-toggler.tsx
+++ src/components/magicui/animated-theme-toggler.tsx
@@ -1,58 +1,41 @@
 "use client";
 
 import { Moon, SunDim } from "lucide-react";
-import { useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { flushSync } from "react-dom";
 import { cn } from "@/lib/utils";
 import { useTheme } from "next-themes";
 
-type props = {
+type Props = {
   className?: string;
 };
 
-export const AnimatedThemeToggler = ({ className }: props) => {
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+export const AnimatedThemeToggler = ({ className }: Props) => {
+  const [mounted, setMounted] = useState(false);
   const buttonRef = useRef<HTMLButtonElement | null>(null);
-
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
 
   const changeTheme = async () => {
     if (!buttonRef.current) return;
 
     await document.startViewTransition(() => {
       flushSync(() => {
-        const dark = document.documentElement.classList.toggle("dark");
-        setIsDarkMode(dark);
-        setTheme(theme == "dark" ? "light" : "dark");
+        setTheme(theme === "dark" ? "light" : "dark");
       });
     }).ready;
-
-    const { top, left, width, height } =
-      buttonRef.current.getBoundingClientRect();
-    const y = top + height / 2;
-    const x = left + width / 2;
-
-    const right = window.innerWidth - left;
-    const bottom = window.innerHeight - top;
-    const maxRad = Math.hypot(Math.max(left, right), Math.max(top, bottom));
-
-    document.documentElement.animate(
-      {
-        clipPath: [
-          `circle(0px at ${x}px ${y}px)`,
-          `circle(${maxRad}px at ${x}px ${y}px)`,
-        ],
-      },
-      {
-        duration: 700,
-        easing: "ease-in-out",
-        pseudoElement: "::view-transition-new(root)",
-      },
-    );
   };
+
   return (
     <button ref={buttonRef} onClick={changeTheme} className={cn(className)}>
-      {isDarkMode ? <SunDim /> : <Moon />}
+      {!mounted ? (
+        <Moon className="opacity-0" />
+      ) : theme === "dark" ? (
+        <SunDim key="sun" />
+      ) : (
+        <Moon key="moon" />
+      )}
     </button>
   );
 };
