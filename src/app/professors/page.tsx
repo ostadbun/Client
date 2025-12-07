e15f727423329f3e6fb09737e32f0e4f448763f6
kazem <mohammadkazemfarahbakhsh5@gmail.com>
2025-12-07 23:11:15 +0330

    changing professoers page adding input from shadcn

--- src/app/professors/page.tsx
+++ src/app/professors/page.tsx
@@ -1,9 +1,12 @@
+import { Input } from '@/components/ui/input';
 import React from 'react';
 
 const professors = () => {
     return (
-        <div>
-            hi professors
+        <div className='w-2/12 place-self-center'>
+            <h1 className='text-4xl place-self-center'>جستوجوی اساتید</h1>
+            <Input className='mt-[2rem]' type="email" placeholder="professors" />
+            
         </div>
     );
 }
--- src/app/subjects/page.tsx
+++ src/app/subjects/page.tsx
@@ -1,51 +1,11 @@
+import { div } from "framer-motion/client";
+
 const subjects = () => {
-    return (
-        <div className="w-7/12 mx-auto">
-            <div className="relative flex f overflow-x-auto dark:bg-gray-50 shadow-sm rounded-lg border border-gray-200">
-
-                <div className="w-full text-sm text-left rtl:text-right text-gray-700">
-                        <div className="flex h-20 bg-amber-300 justify-between">
-                            <button className="px-6 py-3 font-medium rounded-lg border-gray-400 shadow-lg">Product name</button>
-                            <button className="px-6 py-3 font-medium rounded-lg border-gray-400 shadow-lg">Color</button>
-                            <button className="px-6 py-3 font-medium rounded-lg border-gray-400 shadow-lg">Categorbutton</button>
-                            <button className="px-6 py-3 font-medium rounded-lg border-gray-400 shadow-lg">Price</button>
-                            <button className="px-6 py-3 font-medium rounded-lg border-gray-400 shadow-lg">Stock</button>
-                        </div>
-                    <div>
-                        <div className="bg-white border-b flex border-gray-200">
-                            <div className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                Apple MacBook Pro 17"
-                            </div>
-                            <div className="px-6 py-4">Silver</div>
-                            <div className="px-6 py-4">Laptop</div>
-                            <div className="px-6 py-4">$2999</div>
-                            <div className="px-6 py-4">231</div>
-                        </div>
-
-                        <div className="bg-white border-b flex border-gray-200">
-                            <div className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                Microsoft Surface Pro
-                            </div>
-                            <div className="px-6 py-4">White</div>
-                            <div className="px-6 py-4">Laptop PC</div>
-                            <div className="px-6 py-4">$1999</div>
-                            <div className="px-6 py-4">423</div>
-                        </div>
-
-                        <div className="bg-white flex">
-                            <div className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                Magic Mouse 2
-                            </div>
-                            <div className="px-6 py-4">Black</div>
-                            <div className="px-6 py-4">Accessories</div>
-                            <div className="px-6 py-4">$99</div>
-                            <div className="px-6 py-4">121</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    return ( 
+        <div>
+            hi subjects
         </div>
-    );
+     );
 }
 
 export default subjects;
