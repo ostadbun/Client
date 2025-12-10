d45c0202c19a12511965507eb68c3e23c145aba7
kazem <mohammadkazemfarahbakhsh5@gmail.com>
2025-12-10 03:52:35 +0330

    creating card for professors search bar

--- src/app/professors/page.tsx
+++ src/app/professors/page.tsx
@@ -1,12 +1,50 @@
+"use client"
+import { LensDemo } from '@/components/magicui/LensDemo';
+import { Card } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import React from 'react';
+import { number } from 'framer-motion';
+import { Car } from 'lucide-react';
+import React, { useState } from 'react';
 
 const professors = () => {
+    type Professor = {
+        id: number;
+        name: string;
+        field: string;
+        image: string;
+        bio: string;
+    }
+    const [professors, setProfessors] = useState<Professor[]>([
+        {
+            id: 1,
+            name: "دکتر فرحبخش",
+            field: "متخصص بیکار",
+            image: "/images/farhabakhsh.jpg",
+            bio: "بیوگرافی دکتر فرحبخش"
+        },
+        {
+            id: 2,
+            name: "دکتر مسیب نژاد",
+            field: "متخصص گمراه",
+            image: "/images/rezaei.jpg",
+            bio: "بیوگرافی دکتر رضایی"
+        }
+    ])
     return (
         <div className='w-2/12 place-self-center'>
             <h1 className='text-4xl place-self-center'>جستوجوی اساتید</h1>
             <Input className='mt-[2rem]' type="email" placeholder="professors" />
-            
+            <div className='flex justify-center gap-6 mt-20 place-items-center'>
+                {professors.map((c) => {
+                    return <LensDemo
+                        key={c.id}
+                        name={c.name}
+                        field={c.field}
+                        image={c.image}
+                        bio={c.bio}
+                    />
+                })}
+            </div>
         </div>
     );
 }
