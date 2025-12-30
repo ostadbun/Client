6d2d00c4c021315dbfc1c40beb6db6e9b6ad2675
kazem <mohammadkazemfarahbakhsh5@gmail.com>
2025-12-30 20:32:52 +0330

    delete detailanimation

--- src/app/subjects/CourseInfo.tsx
+++ src/app/subjects/CourseInfo.tsx
@@ -20,7 +20,7 @@ const CourseInfo = () => {
         <div className='pt-5 flex justify-center'>
             <div className='w-6/12 h-72 shadow-xl/30 bg-[#131314] rounded-lg'>
                 <h1 className='text-4xl pr-8 pt-5 text-white'>نظریه زبان و ماشین ها</h1>
-                <p className='pt-7 pr-8 text-white'>درس نظریه زبان و ماشین‌ها به بررسی ساختارهای ریاضی برای مدل‌سازی زبان‌ها و ماشین‌های محاسباتی می‌پردازد و مفاهیمی مانند گرامرها، زبان‌های رسمی، اتوماتا و محاسبات‌پذیری را شامل می‌شود.</p>
+                <p className='pt-7 pl-5 pr-8 text-white'>درس نظریه زبان و ماشین‌ها به بررسی ساختارهای ریاضی برای مدل‌سازی زبان‌ها و ماشین‌های محاسباتی می‌پردازد و مفاهیمی مانند گرامرها، زبان‌های رسمی، اتوماتا و محاسبات‌پذیری را شامل می‌شود.</p>
                 <div className='flex justify-center pt-10 gap-7'>
                     <button className='bg-red-600 text-red-100 hover:opacity-85 rounded-lg p-1 ring-2 ring-red-500/50'>سخت</button>
                     <Button className='hover:opacity-85 bg-white text-black'>تخصصی</Button>
@@ -30,8 +30,7 @@ const CourseInfo = () => {
                 </div>
             </div>
             <div className='w-4/12 mr-[4vh] h-[420px] shadow-xl/30 bg-[#131314] rounded-lg'>
-
-            dgbdf
+            <h1>آرا</h1>
 
             </div>
 
