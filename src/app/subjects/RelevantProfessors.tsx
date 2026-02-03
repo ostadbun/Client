ffa0467ecf90db2255b27edb87db4a9e08aedb69
kazem <mohammadkazemfarahbakhsh5@gmail.com>
2026-02-04 00:48:41 +0330

    adding table from shadcn

--- src/app/subjects/RelevantProfessors.tsx
+++ src/app/subjects/RelevantProfessors.tsx
@@ -1,68 +1,101 @@
+"use client"
+
 import React from "react";
 import Link from "next/link";
 
-const RelevantProfessors = () => {
-    const professors = [
-        { id: 1, name: "دکتر احمدی", score: 18.5 },
-        { id: 2, name: "دکتر رضایی", score: 17.2 },
-        { id: 3, name: "دکتر محمدی", score: 19.1 },
-    ];
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+} from "@tanstack/react-table"
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { div } from "framer-motion/client";
+
+
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[]
+    data: TData[]
+}
+
+
+export function RelevantProfessors<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+
+
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+
+
+
     return (
-        <div className="w-5/12 bg-[#181818] border border-gray-500
- shadow-xl/30 rounded-lg p-4">
 
-            <h1 className="text-lg font-bold mb-3 text-white">
-                اساتید مربوطه
-            </h1>
 
-            <input
-                className="w-full mb-4 p-2 rounded-md bg-[#1c1c1e] text-white placeholder-gray-400 outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="جستجو کنید"
-            />
+        <div className="overflow-hidden rounded-md border">
 
-            <div className="overflow-x-auto">
-                <table className="w-full border-collapse mb-[1vh] text-center text-sm text-white">
 
-                    <thead className="bg-[#1c1c1e]">
-                        <tr>
-                            <th className="border border-gray-700 p-2 w-[10%]">ردیف</th>
-                            <th className="border border-gray-700 p-2 w-[40%]">نام استاد</th>
-                            <th className="border border-gray-700 p-2 w-[20%]">نمره کل</th>
-                            <th className="border border-gray-700 p-2 w-[30%]">صفحه استاد</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        {professors.map((prof) => (
-                            <tr
-                                key={prof.id}
-                                className="hover:bg-[#1f1f22] transition"
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => {
+                                return (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )}
+                                    </TableHead>
+                                )
+                            })}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                data-state={row.getIsSelected() && "selected"}
                             >
-                                <td className="border border-gray-700 p-2">
-                                    {prof.id}
-                                </td>
-                                <td className="border border-gray-700 p-2">
-                                    {prof.name}
-                                </td>
-                                <td className="border border-gray-700 p-2">
-                                    {prof.score}
-                                </td>
 
-                                <td className="border border-gray-700 p-2 text-blue-400 hover:underline cursor-pointer">
-                                    <Link href={"/professors"}>
 
-                                        مشاهده
-                                    </Link>
-                                </td>
 
-                            </tr>
-                        ))}
-                    </tbody>
 
-                </table>
-            </div>
 
+                                {row.getVisibleCells().map((cell) => (
+
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell colSpan={columns.length} className="h-24 text-center">
+                                No results.
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
         </div>
+
     );
 };
 
--- src/app/subjects/page.tsx
+++ src/app/subjects/page.tsx
@@ -2,15 +2,62 @@ import CourseInfo from "./CourseInfo";
 import RelevantCourses from "./RelevantCourses";
 import RelevantProfessors from "./RelevantProfessors";
 import Votes from "../professors/Votes";
+import { ColumnDef } from "@tanstack/react-table"
 
+export type RelevantProfessors = {
+    id: number
+    name: string
+    rating: number
+    href: string
+}
 const Subjects = () => {
-    return ( 
+
+
+    // This type is used to define the shape of our data.
+    // You can use a Zod schema here if you want.
+
+    const columns: ColumnDef<RelevantProfessors>[] = [
+        {
+            accessorKey: "id",
+            header: "رقم",
+        },
+        {
+            accessorKey: "name",
+            header: "نام استاد",
+        },
+        {
+            accessorKey: "rating",
+            header: "نمره کل",
+        },
+        {
+            accessorKey: "href",
+            header: "صفحه استاد",
+        },
+    ]
+
+
+
+    const data: RelevantProfessors[] = [
+        {
+            href: "a", id: 1, name: "مدائنی", rating: 2
+        },
+        {
+            href: "b", id: 2, name: "اردوخانی", rating: 32
+        },
+        {
+            href: "c", id: 3, name: "جلالی", rating: 244
+        },
+        {
+            href: "d", id: 4, name: "احسانی", rating: 25550
+        },
+    ]
+    return (
         <div className="flex flex-wrap justify-center gap-x-4 gap-y-14 lg:px-0 md:gap-x-16">
-            <CourseInfo/>
-            <RelevantProfessors/>
-            <RelevantCourses/>
+            <CourseInfo />
+            <RelevantProfessors columns={columns} data={data} />
+            <RelevantCourses />
         </div>
-     );
+    );
 }
 
 export default Subjects;
