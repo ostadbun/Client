f251911579e3a598ef04ab2c28c6620b21fc92aa
kazem <mohammadkazemfarahbakhsh5@gmail.com>
2026-02-13 21:13:19 +0330

    revolution

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,20 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar"
+
+export function AppSidebar() {
+  return (
+    <Sidebar dir="rtl" side="right">
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
