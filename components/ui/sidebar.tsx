2425363f944f30b893e79ca97cfb52ae7c33e780
kazem <mohammadkazemfarahbakhsh5@gmail.com>
2026-02-14 23:16:25 +0330

    some minimal update

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -10,6 +10,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
+
 import {
   Sheet,
   SheetContent,
