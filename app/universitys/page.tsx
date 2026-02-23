1bd478f9474e8a6495f0d49d1d13b1fb68302743
kazem <mohammadkazemfarahbakhsh5@gmail.com>
2026-02-23 06:18:24 +0330

    add table to university

--- app/universitys/page.tsx
+++ app/universitys/page.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import { DataTable } from "@/components/data-table"
+import { columns, Payment } from "./columns"
 
-const Page = () => {
-    return (
-        <div>
-            fvfdv
-        </div>
-    );
+async function getData(): Promise<Payment[]> {
+  return [
+    { status: "Success", email: "ken99@example.com", amount: 316 },
+    { status: "Success", email: "abe45@example.com", amount: 242 },
+    { status: "Processing", email: "monserrat44@example.com", amount: 837 },
+    { status: "Success", email: "silas22@example.com", amount: 874 },
+    { status: "Failed", email: "carmella@example.com", amount: 721 },
+  ]
 }
 
-export default Page;
+export default async function Page() {
+  const data = await getData()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
